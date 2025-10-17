28918250453b46cc6c65e6817c5ec5ca89c8db76
Mostafa Ebrahimi <mst244831@gmail.com>
2025-10-17 11:32:56 +0330

    Routing Procces | Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckOutPage from "./pages/CheckOutPage";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to={"/products"} replace />} />
+      <Route path="/products" element={<ProductsPage />} />
+      <Route path="/products/:id" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckOutPage />} />
+      <Route path="/*" element={<PageNotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
